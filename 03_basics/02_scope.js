cce3d83f8f8c0dde721ced6da09387b00958eb71
rudrapratap2104 <rudrpratap.study@gmail.com>
2026-05-19 04:24:36 +0000

    vid22 done

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,10 +5,50 @@ if (true) {
     let a = 1
     const b = 2
 // var c = 3
-console.log("Inner:",a )
+// console.log("Inner:",a )
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+    const username = "John"
+    console.log(username)
+    
+        function two() {
+            const website = "example.com"
+            console.log(username);
+        }
+    
+    // console.log(website);
+    
+    two()
+            
+}
+
+// one()
+
+if (true) {
+    const username = "Rudra"
+    if(username === "Rudra") {
+        const website = " example.com"
+        // console.log(username + website);
+    }
+}
+
+// ----------interesting---------------
+
+console.log(addOne(5));
+
+function addOne(num){
+    return num + 1
+}
+
+
+console.log(addTwo(7)); //error trying to use variable before declaration
+
+const addTwo = function(num) {
+    return num + 2
+}
