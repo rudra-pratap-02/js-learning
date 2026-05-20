bbe577ea6231fed91adb9b41c6cb49d6dd2a390f
rudrapratap2104 <rudrpratap.study@gmail.com>
2026-05-20 13:37:27 +0000

    vid26.2 done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch(key){
+//     case value1:
+//         code block
+//         break;
+//     case value2:
+//          code block
+//         break;
+//     default:
+//          code block
+// }
+
+const day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+
+    default:
+        console.log("default case");
+        
+        break;
+}
