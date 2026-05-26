ba9369fef79703f9e1409db1efd68e4cfb12b962
rudra-pratap-02 <rudrpratap.study@gmail.com>
2026-05-26 07:41:37 +0000

    vid28 done

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+
+let index = 1
+// while (index <= 10) {
+//     console.log(index)
+//     index+=2
+// }
+
+let myArray = ["flash", "superman", "batman", "wonder woman"]
+let arrayIndex = 0
+// while (arrayIndex < myArray.length) {
+//     console.log(myArray[arrayIndex])
+//     arrayIndex++
+// }
+ 
+
+let score = 5
+do {
+    console.log(`score: ${score}`)
+    score++
+} while (score < 5)
