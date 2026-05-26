b476ebc327b683a75a6f4794c39aff7306c7474f
rudra-pratap-02 <rudrpratap.study@gmail.com>
2026-05-26 07:59:12 +0000

    vid29.1 done

---
+++ 05_iterations/three.js
@@ -0,0 +1,20 @@
+//  for of
+
+// ["","", ""]
+// [{}, {}, {}]
+
+const array = ["a", "b", "c"];
+
+// for (const num of array) {
+//     console.log(num);
+    
+// }
+
+const greeting = "Hello";
+
+for (const greet of greeting) {
+    console.log(`Each char is ${greet}`);
+    
+}
+
+for (const char of "Hello") {
