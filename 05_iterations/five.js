a9aae2d2d9746c90a48c6d8d504ea6094b1e8532
rudra-pratap-02 <rudrpratap.study@gmail.com>
2026-05-28 04:50:48 +0000

    vid29 done completely

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -4,6 +4,37 @@ const coding = ['JavaScript', 'Python', 'Ruby', 'Go', 'C++']
 //     console.log(item);
 // });     
 
-coding.forEach((item) => {
-    console.log(item);
+// coding.forEach((item) => {
+//     console.log(item);
+// });
+
+// function printing(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printing);
+
+// coding.forEach((item,index,arr) => {
+//     console.log(index, item, arr);
+    
+// })
+
+const coding2 = [
+    {
+        languageName: 'JavaScript',
+        languageFileName : 'js'
+    },
+    {
+        languageName: 'C++',
+        languageFileName : 'cpp'
+    },
+    {
+        languageName: 'Ruby',
+        languageFileName : 'rb'
+    },
+]
+
+coding2.forEach((item) => {
+    console.log(item.languageName);
+    
 });
