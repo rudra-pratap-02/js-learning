d682d92083c3dcd3067b0820f72ed2ad2c60a1f9
rudra-pratap-02 <rudrpratap.study@gmail.com>
2026-05-28 05:05:15 +0000

    vid30.1 done

---
+++ 05_iterations/six.js
@@ -0,0 +1,16 @@
+const coding = ['JavaScript', 'Python', 'Ruby', 'Go', 'C++']
+
+// const value =coding.forEach( (item) => {
+//     // console.log(item);
+//     return item;
+// })
+// // forEach does not return anything
+
+// console.log(value);
+
+const myNums = [1, 2, 3, 4, 5]
+
+const i = myNums.filter( (item) => { 
+    return item > 3
+})
+console.log(i);
