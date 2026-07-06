108f11593c5ea67c6b8c01713afdebf78c025421
rudra-pratap-02 <rudrpratap.study@gmail.com>
2026-07-06 16:00:44 +0000

    vid30.2 done

---
+++ 05_iterations/seven.js
@@ -0,0 +1,8 @@
+const myNums = [1, 2, 3, 4, 5]
+
+// const i = myNums.map( (num) => { return num *5 })
+
+
+
+
+console.log(i);
--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,16 +1,25 @@
-const coding = ['JavaScript', 'Python', 'Ruby', 'Go', 'C++']
+// const coding = ['JavaScript', 'Python', 'Ruby', 'Go', 'C++']
 
 // const value =coding.forEach( (item) => {
-//     // console.log(item);
+//     console.log(item);
 //     return item;
 // })
-// // forEach does not return anything
 
 // console.log(value);
+// forEach does not return anything
+
 
 const myNums = [1, 2, 3, 4, 5]
 
 const i = myNums.filter( (item) => { 
-    return item > 3
+    return item > 3})
+// console.log(i);
+
+newNums = []
+
+myNums.forEach( (item) => {
+    if (item<5){
+        newNums.push(item)
+    }
 })
-console.log(i);
+console.log(newNums);
